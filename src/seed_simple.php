d63c87c340c36d89bec20fc3fb62b41b464a3407
chainaccelerator <39132119+chainaccelerator@users.noreply.github.com>
2019-02-11 22:08:26 +0100

    Start restore feature
    Start restore feature

--- src/seed_simple.php
+++ src/seed_simple.php
@@ -17,15 +17,12 @@ trait Seed_simple
     private static $SEED_INVALID_ENTROPY = 'Invalid entropy';
     private static $SEED_INVALID_CHECKSUM = 'Invalid mnemonic checksum';
 
-    private static $seed_file_tmp = '../data/seed/key_private.seed';
-
     private static $salt_prefix = 'mnemonic';
     private static $entropy_algo_size = 128;
     private static $entropy_algo = 'sha512'; // 128 bits
 
-    public $password;
-    public $word_list = array();
-    public $word_list_count = 0;
+    public $word_list;
+    public $mnemonic;
 
     public function seed_init(string $wordlist_file)
     {
@@ -34,39 +31,55 @@ trait Seed_simple
         return true;
     }
 
-    static public function bitreverse($bitstring, $size)
+    public function mnemonic_gen()
     {
-
-        $mask = ~0;
-        while ($size > 0) {
-
-            $size = $size >> 1;
-            $mask = $mask ^ ($mask << $size);
-            $bitstring = (($bitstring >> $size) & $mask) | ((~$mask) & ($bitstring << $size));
-        }
-        return $bitstring;
-    }
-
-    function mnemonic_gen()
-    {
-
-        $word_list = array();
+        $mnemonic = array();
+        $grain = self::grain_get();
         $rsa_private_key = self::rsa_private_key_get();
-        $rsa_private_key_master = hash(self::entropy_algo, $rsa_private_key, true);
+        $rsa_private_key_master = hash(self::entropy_algo, $rsa_private_key . $grain, true);
         $rsa_private_key_master_bin_str = sprintf("%04b", $rsa_private_key_master);
         $checksum_str = substr($rsa_private_key_master_bin_str, -4);
         list($checksum) = sscanf($checksum_str, '%04b');
-        $entropy132bits = $rsa_private_key_master . $checksum; // @todo concat
+        $entropy132bits = $rsa_private_key_master . $checksum;
 
-        $entropy132bits_bin_str = sprintf("%04b", $entropy132bits);
-        $entropy132bits_bin_str_parts = str_split($entropy132bits_bin_str, 1);
+        $entropy132bits_bin_str = sprintf("%132b", $entropy132bits);
+        $entropy132bits_bin_str_parts = str_split($entropy132bits_bin_str, 11);
 
         foreach ($entropy132bits_bin_str_parts as $entropy132bits_bin_str_part) {
 
-            $word_list[] = $this->word_list[$entropy132bits_bin_str_part];
+            list($index_bin) = sscanf($entropy132bits_bin_str_part, '%11b');
+            $index_dec = bindec($index_bin);
+            $mnemonic[] = $this->word_list[$index_dec];
         }
-        file_put_contents(self::$seed_file_tmp, implode('', $word_list));
+        $this->mnemonic = implode(' ', $mnemonic);
 
-        return true;
+        return $this->mnemonic;
+    }
+
+    private static function grain_get()
+    {
+        $grain = file_get_contents(self::$seed_grain_file);
+
+        return $grain;
+    }
+
+    public function world_list_get()
+    {
+
+        return $this->word_list;
+    }
+
+    public function private_key_restore($mnemonic)
+    {
+
+        $this->mnemonic = $mnemonic;
+        $mnemonic_array = explode(' ', $this->mnemonic);
+
+        foreach ($mnemonic_array as $word) {
+
+            $word_bin_str = sprintf("%11b", $word);
+            list($word_bin) = sscanf($word_bin_str, '%11b');
+
+        }
     }
 }
